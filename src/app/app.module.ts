d18060a2458d9ded7e070ede30e199f4260f6bfb
Balwinder Katoch <Balwinder Katoch>
2016-10-27 18:08:49 -0400

    chng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
+import {TabViewModule} from 'primeng/primeng'; 
+
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -20,6 +22,7 @@ import { removeNgStyles, createNewHosts } from '@angularclass/hmr';
     BrowserModule,
     HttpModule,
     FormsModule,
+    TabViewModule,
     routing
   ],
   declarations: [
